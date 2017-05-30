d50815d9c8f5c1fc36e1c59af97cb78beda55807
Nick Brierley <nickcbrierley@gmail.com>
2017-05-30 17:13:26 +0000

    checkpoint3 completed

--- scripts/blocpong.js
+++ scripts/blocpong.js
@@ -8,11 +8,12 @@ function Canvas(x, y, width, height){
     this.height = height;
 }
     
-function Paddle(x, y, width, height) {
+function Paddle(x, y, width, height, speed) {
     this.paddlex = x;
     this.paddley = y;
     this.width = width;
     this.height = height;
+    this.speed = speed;
 }
 
 function Ball(x, y, width, height){
@@ -26,23 +27,31 @@ Canvas.prototype.render = function() {
     context.beginPath();
     context.fillStyle = 'gray';
     context.fillRect(this.canvasx, this.canvasy, this.width, this.height);
-}
+};
 
 Paddle.prototype.render = function() {
     context.beginPath();
-    context.fillStyle = 'black'
+    context.fillStyle = 'black';
     context.fillRect(this.paddlex, this.paddley, this.width, this.height);
+};
+
+Paddle.prototype.move = function(key) {
+    var keypress = key.keyCode;
+    if (keypress == 38 || keypress == 87 && this.paddley > 0) {
+        this.paddley = this.paddley - this.speed;
+    } else if (keypress === 40 || keypress == 83 && this.paddley < 380) {
+        this.paddley = this.paddley + this.speed;
+    }
 }
 
 Ball.prototype.render = function() {
     context.beginPath();
     context.fillStyle = 'black';
     context.fillRect(this.ballx, this.bally, this.width, this.height);
-}
-
+};
 var canvas = new Canvas(0,0,640,480);
-var player = new Paddle(10,190,20,100);
-var computer = new Paddle(610,190,20,100);
+var player = new Paddle(10,190,20,100,5);
+var computer = new Paddle(610,190,20,100,5);
 var ball = new Ball(320,240,20,20);
 
 var render = function() {
@@ -50,9 +59,21 @@ var render = function() {
     player.render();
     computer.render();
     ball.render();
-}
+};
 
-
-window.onload =function() {
+var animate = window.requestAnimationFrame ||
+              function(callback) { window.setTimeout(callback, 1000/60) };
+              
+function step(){
     render();
+    animate(step);
 }
+
+var moveplayer = function(key) {
+    player.move(key);
+};
+
+window.onload = function() {
+     step();
+     window.addEventListener('keydown', moveplayer);
+};
